0a2b018dafa4821f5b341f1d4da4b90c9e84d327
Diego Vargas <diego.vargas.j@ing.uchile.cl>
2024-09-11 09:44:01 -0300

    Creating QR code model

---
+++ models/qr.go
@@ -0,0 +1,80 @@
+package models
+
+import (
+	"errors"
+	"fmt"
+	"net/mail"
+	"regexp"
+	"strings"
+	"time"
+
+	log "github.com/gophish/gophish/logger"
+	"github.com/jinzhu/gorm"
+	"github.com/sirupsen/logrus"
+)
+
+// QR contains the settings for generating QR codes
+type QR struct {
+	UserId		 int64	   `json:"user_id"`
+	Height       int64     `json:"height"`
+	Width        int64     `json:"width"`
+	Pixels       string    `json:"pixels"`
+	Background   string    `json:"background"`
+}
+
+// ErrQRCodeTooSmall is thrown when QR code dimensions are too small for a QR code
+var ErrQRCodeTooSmall = errors.New("QR Code is too small")
+
+// ErrInvalidColor is thrown when an invalid color is chosen for a QR code
+var ErrInvalidColor = errors.New("Invalid color")
+
+// Validate performs validation on given settings
+func (qr *QR) Validate() error {
+	if qr.Height < 64 || qr.Width < 64 {
+		return ErrQRCodeTooSmall
+	}
+	r, _ := regexp.MustCompile("^#[[:xdigit:]]{3,6}$")
+	if !r.MatchString(qr.Pixels) || !r.MatchString(qr.Background) {
+		return ErrInvalidColor
+	}
+	return nil
+}
+
+// GetQR returns the QR settings
+func GetQR(uid int64) (QR, error) {
+	qr := QR{}
+	err := db.Where("user_id=?", uid).Find(&qr).Error
+	if err != nil {
+		log.Error(err)
+	}
+	return g, err
+}
+
+// DeleteQR deletes que QR code settings
+func DeleteQR(uid int64) error {
+	qr := QR{}
+	err := db.Where("user_id=?", uid).Find(&qr).Error
+	if err != nil {
+		log.Error(err)
+	}
+	return err
+}
+
+// UpdateQR updates the QR code settings
+func UpdateQR(qr *QR, uid int64) error {
+	err := qr.Validate()
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	err = DeleteQR(uid)
+	if err != nil {
+		log.Error(err)
+		return err
+	}
+	err = db.Save(qr).Error
+	if err != nil {
+		log.Error(err)
+	}
+	return err
+}
